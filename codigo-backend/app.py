8fead72d9fed305339ac8f9980ba0e547bc0b493
miguel20083779 <miguel.restrepo794@asys.edu.co>
2026-06-24 12:13:25 -0500

    Refactor app.py to include path and app import
    Added sys.path configuration and app import for Flask.

--- codigo-backend/app.py
+++ codigo-backend/app.py
@@ -1,6 +1,13 @@
 import os
+import sys
 
+# Esto le enseña a Python a buscar dentro de tu estructura de carpetas
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
-if __name__ == '__main__':
+# Importamos la aplicación de Flask desde tu archivo interno
+from app import app
+
+if __name__ == "__main__":
+    # Render necesita obligatoriamente tomar el puerto dinámico de esta variable
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
