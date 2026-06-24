ebda3c8f8a92ddef96491aa2597db7c772f074a6
miguel20083779 <miguel.restrepo794@asys.edu.co>
2026-06-24 12:16:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-from app.backend.app import *
+import os
+from flask import Flask
+
+# Aquí creamos el motor directamente para evitar problemas de carpetas
+app = Flask(__name__)
+
+# ==========================================
+# ¡RUTAS DE PRUEBA / BIENVENIDA!
+# ==========================================
+@app.route('/')
+def home():
+    return "¡Servidor de GridStock funcionando correctamente en Render!"
+
+# Si tu código real tiene rutas como @app.route('/ventas'), etc.,
+# puedes copiarlas y pegarlas aquí abajo.
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
